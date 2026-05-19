8a10d86f583e7a3676490b516db0fa6271b6926a
Chaman Narved <chamannarved@gmail.com>
2026-05-19 19:34:24 +0530

    Add dynamic current year

--- js/main.js
+++ js/main.js
@@ -2,22 +2,25 @@ const menuBtn = document.querySelector(".menu-btn");
 const menuSection = document.querySelector(".menu-section");
 const closeBtn = document.querySelector('.btn-close');
 
+const currentYear = new Date().getFullYear();
+document.getElementById('year').textContent = currentYear;
+
 menuBtn.addEventListener("click", function() {
-    menuSection.classList.remove("hide-menu");
-    menuSection.classList.add("show-menu");
+  menuSection.classList.remove("hide-menu");
+  menuSection.classList.add("show-menu");
 });
 
 closeBtn.addEventListener('click', function() {
-    menuSection.classList.remove("show-menu");
-    menuSection.classList.add("hide-menu");
+  menuSection.classList.remove("show-menu");
+  menuSection.classList.add("hide-menu");
 });
 
 window.addEventListener("DOMContentLoaded", function() {
-    if (window.innerWidth > 600) {
-        menuSection.classList.remove("hide-menu");
-    } else {
-        menuSection.classList.add("hide-menu");
-    }
+  if (window.innerWidth > 600) {
+    menuSection.classList.remove("hide-menu");
+  } else {
+    menuSection.classList.add("hide-menu");
+  }
 });
 
 // const themeBtn = document.querySelector(".theme-btn");
